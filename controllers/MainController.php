96276a3f2b705310cc600ba81d9f903f1eb8d248
юлиана яроцкая <yarotzckaya.yuliana@yandex.ua>
2018-07-04 10:58:45 +0300

    SQL requests works

--- controllers/MainController.php
+++ controllers/MainController.php
@@ -10,7 +10,10 @@ class MainController extends Controller
 	public function indexAction()
 	{
 	   $db = new DB;
-	   $data = $db->row('SELECT logingit FROM users');
+
+	   $form = 1;
+
+	   $data = $db->column('SELECT login FROM users WHERE id = '.$form);
 	   debug($data);
 	    $this->view->render('Min framework', $db);
 
--- lib/Db.php
+++ lib/Db.php
@@ -27,7 +27,7 @@ class Db
     public function row($sql)
     {
         $result = $this->query($sql);
-        return $result->fetchAll();
+        return $result->fetchAll(PDO::FETCH_ASSOC);
     }
 
     public function column($sql)
